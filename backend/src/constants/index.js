55d638bb07373008e71252b7d88e60115ababf71
Ey0s <eyosyasyosef1934@gmail.com>
2026-03-28 20:38:37 +0300

    add index.js

---
+++ backend/src/constants/index.js
@@ -0,0 +1,21 @@
+exports.ROLES = {
+    ADMIN: 'admin',
+    DOCTOR: 'doctor',
+    NURSE: 'nurse',
+    RECEPTIONIST: 'receptionist',
+    PATIENT: 'patient'
+};
+
+exports.APPOINTMENT_STATUS = {
+    PENDING: 'Pending',
+    CONFIRMED: 'Confirmed',
+    CANCELLED: 'Cancelled',
+    COMPLETED: 'Completed'
+};
+
+exports.INVOICE_STATUS = {
+    UNPAID: 'Unpaid',
+    PARTIALLY_PAID: 'Partially Paid',
+    PAID: 'Paid',
+    CANCELLED: 'Cancelled'
+};
